3ac342f9b8c3a8b0d2f29a4f69d0f3e4962b5d63
Muhammed Emin Boydak <emnbydk@gmail.com>
2024-11-13 00:14:41 +0300

    feat: started integrating MainArea and Footer components in page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
+import Footer from "@/components/Footer";
+import MainArea from "@/components/MainArea";
+
 export default function Home() {
   return (
-    <div>
-      
+    <div className="grid items-center justify-items-center min-h-screen">
+      <main className="flex flex-col items-center">
+        <MainArea />
+      </main>
+      <footer className="flex">
+        <Footer />
+      </footer>
     </div>
   );
 }
